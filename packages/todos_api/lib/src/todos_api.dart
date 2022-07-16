49bb34edb9964c663c91b1baa4763fd491bea539
bala <vishnubalu.sangem@gmail.com>
2022-07-16 20:51:59 +0530

    Initialize todo api repository

---
+++ packages/todos_api/lib/src/todos_api.dart
@@ -0,0 +1,7 @@
+/// {@template todos_api}
+/// API package with interface and modesl for providing access to todos
+/// {@endtemplate}
+class TodosApi {
+  /// {@macro todos_api}
+  const TodosApi();
+}

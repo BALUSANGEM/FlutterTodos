e2648ff7fcae6c911d881e206d7e2e2a25a9260b
bala <vishnubalu.sangem@gmail.com>
2022-07-18 14:35:13 +0530

    Load todos on initializing local storage

--- packages/local_storage_todos_api/lib/src/local_storage_todos_api.dart
+++ packages/local_storage_todos_api/lib/src/local_storage_todos_api.dart
@@ -1,7 +1,34 @@
-/// {@template local_storage_todos_api}
-/// A Flutter implementation of the TodosApi that uses local storage.
-/// {@endtemplate}
-class LocalStorageTodosApi {
-  /// {@macro local_storage_todos_api}
-  const LocalStorageTodosApi();
+import 'dart:convert';
+
+import 'package:rxdart/subjects.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:todos_api/todos_api.dart';
+
+class LocalStorageTodosApi extends TodosApi {
+  LocalStorageTodosApi({
+    required SharedPreferences plugin,
+  }) : _plugin = plugin {
+    _init();
+  }
+
+  final SharedPreferences _plugin;
+
+  final _todoStreamController = BehaviorSubject<List<Todo>>.seeded(const []);
+
+  static const tododsCollectionKey = 'tods_collection_key';
+
+  String? _getValue(String key) => _plugin.getString(key);
+
+  void _init() {
+    final todosJson = _getValue(tododsCollectionKey);
+    if (todosJson != null) {
+      final todos = List<Map>.from(json.decode(todosJson) as List)
+          .map((jsonMap) => Todo.fromJson(Map<String, dynamic>.from(jsonMap)))
+          .toList();
+      _todoStreamController.add(todos);
+    } else {
+      _todoStreamController.add(const []);
+    }
+  }
+
 }

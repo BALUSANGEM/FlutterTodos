1eb6a861109c9358d348ca3151b3398a7b8fd981
bala <vishnubalu.sangem@gmail.com>
2022-07-21 18:38:15 +0530

    Adds bloc events for todo overview feature

---
+++ lib/todos_overview/bloc/todos_overview_event.dart
@@ -0,0 +1,33 @@
+import 'package:todos_api/todos_api.dart';
+
+abstract class TodosOverviewEvent {}
+
+class TodosOverviewSubscriptionRequested extends TodosOverviewEvent {
+  TodosOverviewSubscriptionRequested();
+}
+
+class TodosOverviewCompletionToggled extends TodosOverviewEvent {
+  TodosOverviewCompletionToggled(
+      {required this.todo, required this.isCompleted});
+
+  final Todo todo;
+  final bool isCompleted;
+}
+
+class TodosOverviewTodoDeleted extends TodosOverviewEvent {
+  TodosOverviewTodoDeleted({required this.todo});
+
+  final Todo todo;
+}
+
+class TodosOverviewUndoDeletionRequested extends TodosOverviewEvent {
+  TodosOverviewUndoDeletionRequested();
+}
+
+class TodosOverviewToggleAllRequested extends TodosOverviewEvent {
+  TodosOverviewToggleAllRequested();
+}
+
+class TodosOverviewClearCompletedRequested extends TodosOverviewEvent {
+  TodosOverviewClearCompletedRequested();
+}

634e36cfd81d87b78763cf07345c43e73e205e16
bala <vishnubalu.sangem@gmail.com>
2022-07-24 20:51:58 +0530

    Creates Edit todos state

--- lib/edit_todo/bloc/edit_todo_bloc.dart
+++ lib/edit_todo/bloc/edit_todo_bloc.dart
@@ -1,3 +1,6 @@
+import 'package:equatable/equatable.dart';
+import 'package:todos_api/todos_api.dart';
+
 part 'edit_todo_event.dart';
 
 part 'edit_todo_state.dart';
--- lib/edit_todo/bloc/edit_todo_state.dart
+++ lib/edit_todo/bloc/edit_todo_state.dart
@@ -1,1 +1,33 @@
 part of 'edit_todo_bloc.dart';
+
+enum EditTodoStatus { initial, loading, success, failure }
+
+class EditTodoState extends Equatable {
+  const EditTodoState(
+      {this.status = EditTodoStatus.initial,
+      this.initialTodo,
+      this.title = '',
+      this.description = ''});
+
+  final EditTodoStatus status;
+  final Todo? initialTodo;
+  final String title;
+  final String description;
+
+  @override
+  List<Object?> get props => [status, initialTodo, title, description];
+
+  EditTodoState copyWith({
+    EditTodoStatus? status,
+    Todo? initialTodo,
+    String? title,
+    String? description,
+  }) {
+    return EditTodoState(
+      status: status ?? this.status,
+      initialTodo: initialTodo ?? this.initialTodo,
+      title: title ?? this.title,
+      description: description ?? this.description,
+    );
+  }
+}

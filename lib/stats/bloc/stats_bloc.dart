5072d63c1af8d3436dab4d2fe893607924dde3eb
bala <vishnubalu.sangem@gmail.com>
2022-07-24 20:23:57 +0530

    Creates Stats bloc and Implements onStatsRequested event handler to emit active and completed todos count

--- lib/stats/bloc/stats_bloc.dart
+++ lib/stats/bloc/stats_bloc.dart
@@ -1,5 +1,38 @@
+import 'dart:async';
+
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todos_repository/todos_repository.dart';
 
 part 'stats_events.dart';
 
 part 'stats_state.dart';
+
+class StatsBloc extends Bloc<StatsEvent, StatsState> {
+  StatsBloc({
+    required TodosRepository todoRepository,
+  })  : _todosRepository = todoRepository,
+        super(const StatsState()) {
+    on<StatsSubscriptionRequested>(_onStatsRequested);
+  }
+
+  final TodosRepository _todosRepository;
+
+  Future<void> _onStatsRequested(
+    StatsSubscriptionRequested event,
+    Emitter<StatsState> emit,
+  ) async {
+    emit(state.copyWith(status: StatsStatus.loading));
+    await emit.forEach<List<Todo>>(
+      _todosRepository.getTodos(),
+      onData: (todos) => state.copyWith(
+        status: StatsStatus.success,
+        activeTodos: todos.where((todo) => !todo.isCompleted).length,
+        completedTodos: todos.where((todo) => todo.isCompleted).length,
+      ),
+      onError: (_, __) => state.copyWith(
+        status: StatsStatus.failure,
+      ),
+    );
+  }
+}

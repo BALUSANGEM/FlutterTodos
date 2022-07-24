9464c9fc5e1a1df915d6ce89587e83efdb5d2be7
bala <vishnubalu.sangem@gmail.com>
2022-07-24 21:00:30 +0530

    Creates edit todo bloc and empty event handlers

--- lib/edit_todo/bloc/edit_todo_bloc.dart
+++ lib/edit_todo/bloc/edit_todo_bloc.dart
@@ -1,6 +1,45 @@
+import 'dart:async';
+
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todos_api/todos_api.dart';
+import 'package:todos_repository/todos_repository.dart';
 
 part 'edit_todo_event.dart';
 
 part 'edit_todo_state.dart';
+
+class EditTodoBloc extends Bloc<EditTodoEvent, EditTodoState> {
+  EditTodoBloc({
+    required TodosRepository todosRepository,
+    required Todo? initialTodo,
+  })  : _todosRepository = todosRepository,
+        super(
+          EditTodoState(
+            initialTodo: initialTodo,
+            title: initialTodo?.title ?? '',
+            description: initialTodo?.description ?? '',
+          ),
+        ) {
+    on<EditTodoTitleChanged>(_onTitleChanged);
+    on<EditTodoDescriptionChanged>(_onDescriptionChanged);
+    on<EditTodoSubmitted>(_onSubmitted);
+  }
+
+  final TodosRepository _todosRepository;
+
+  FutureOr<void> _onTitleChanged(
+    EditTodoTitleChanged event,
+    Emitter<EditTodoState> emit,
+  ) {}
+
+  FutureOr<void> _onDescriptionChanged(
+    EditTodoDescriptionChanged event,
+    Emitter<EditTodoState> emit,
+  ) {}
+
+  FutureOr<void> _onSubmitted(
+    EditTodoSubmitted event,
+    Emitter<EditTodoState> emit,
+  ) {}
+}

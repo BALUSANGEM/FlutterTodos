9d8d12bd8749f31cfe93ae59f4972aa6842bbaf4
bala <vishnubalu.sangem@gmail.com>
2022-07-24 20:31:33 +0530

    Creates stats page template

---
+++ lib/stats/view/stats_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_todos/stats/bloc/stats_bloc.dart';
+import 'package:todos_repository/todos_repository.dart';
+
+class StatsPage extends StatelessWidget {
+  const StatsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => StatsBloc(
+        todoRepository: context.read<TodosRepository>(),
+      )..add(const StatsSubscriptionRequested()),
+      child: const StatsView(),
+    );
+  }
+}
+
+class StatsView extends StatelessWidget {
+  const StatsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final state = context.watch<StatsBloc>().state;
+    return Scaffold();
+  }
+}

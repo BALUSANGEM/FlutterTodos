23205dd158045a65026e059a199f93f449ebaa6e
bala <vishnubalu.sangem@gmail.com>
2022-07-27 22:27:27 +0530

    Implements Filtering todos based on selected todo overview filter

--- lib/todos_overview/model/todo_overview_filter.dart
+++ lib/todos_overview/model/todo_overview_filter.dart
@@ -1,2 +1,20 @@
+import 'package:todos_repository/todos_repository.dart';
+
 enum TodoOverViewFilter { all, activeOnly, completedOnly }
 
+extension TodosViewFilterX on TodoOverViewFilter {
+  bool apply(Todo todo) {
+    switch (this) {
+      case TodoOverViewFilter.all:
+        return true;
+      case TodoOverViewFilter.activeOnly:
+        return !todo.isCompleted;
+      case TodoOverViewFilter.completedOnly:
+        return todo.isCompleted;
+    }
+  }
+
+  Iterable<Todo> applyAll(Iterable<Todo> todos) {
+    return todos.where(apply);
+  }
+}

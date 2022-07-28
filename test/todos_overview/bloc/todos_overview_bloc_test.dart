8bd8b12bfb4d26394a6b5b550aef03f9866a838f
bala <vishnubalu.sangem@gmail.com>
2022-07-28 21:51:29 +0530

    Adds Test for When subscription called gettodos stream

--- test/todos_overview/bloc/todos_overview_bloc_test.dart
+++ test/todos_overview/bloc/todos_overview_bloc_test.dart
@@ -1,4 +1,6 @@
+import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_todos/todos_overview/bloc/todos_overview_bloc.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:todos_repository/todos_repository.dart';
 
@@ -19,6 +21,21 @@ void main() {
       when(() => todosRepository.saveTodo(any())).thenAnswer((_) async {});
       when(() => todosRepository.getTodos())
           .thenAnswer((_) => Stream.value(mockTodos));
+    });
+
+    TodosOverviewBloc buildBloc() {
+      return TodosOverviewBloc(todosRepository: todosRepository);
+    }
+
+    group('TodosOverviewSubscriptionRequested', () {
+      blocTest<TodosOverviewBloc, TodosOverViewState>(
+        'Start listening to repository get todos stream',
+        build: buildBloc,
+        act: (bloc) => bloc.add(const TodosOverviewSubscriptionRequested()),
+        verify: (_) {
+          verify(() => todosRepository.getTodos()).called(1);
+        },
+      );
     });
   });
 }
